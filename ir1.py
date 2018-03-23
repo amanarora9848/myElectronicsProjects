290ac2417cdd3dca559decce80e33001f3e045ce
Aman Arora <aman.arora9848@gmail.com>
2018-03-23 20:00:52 +0530

    Update ir1.py
    The project has been coded in Python language and is for raspberry pi 3, though the user can change thee pins and use it for other pi models too.

--- ir1.py
+++ ir1.py
@@ -1,25 +1,25 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD) #sets the gpio mode to board, that is, board pin number will be followed.
 
-ir = 12
-led1 = 16
-led2 = 18
+ir = 12 #sets pin 12 on the board for receiving infrared sersor's output.
+led1 = 16 #sets pin 16 on the pi board for giving output to one led.
+led2 = 18 #sets pin 18 as output for another led.
 
-GPIO.setup(ir, GPIO.IN)
-GPIO.setup(led1, GPIO.OUT)
-GPIO.setup(led2, GPIO.OUT)
+GPIO.setup(ir, GPIO.IN) #ir is setup as input pin.
+GPIO.setup(led1, GPIO.OUT) #led1 as output pin to light it up as per the code.
+GPIO.setup(led2, GPIO.OUT) #led2 as output pin to light it up as per the code.
 
 def perform():
 	while GPIO.input(ir) == 0:
-		print("An interruption noted.")
+		print("An interruption noted.") # As soon as ir stops receiving back the infrared rays, light up led at pin 16.
 		GPIO.output(led1, GPIO.HIGH)
-		GPIO.output(led2, GPIO.LOW)
-		time.sleep(0.5)
+		GPIO.output(led2, GPIO.LOW) # Keep led at pin 18 off.
+		time.sleep(0.5) #Wait for 0.5 seconds.
 	while GPIO.input(ir) == 1:
 		print("Good to go. No obstacle.")
-		GPIO.output(led2, GPIO.HIGH)
-		GPIO.output(led1, GPIO.LOW)
+		GPIO.output(led2, GPIO.HIGH) # As soon as ir receives back the infrared rays, light up led at pin 18.
+		GPIO.output(led1, GPIO.LOW) #Keep led at pin 16 off.
 		time.sleep(0.5)
 
 if __name__ == '__main__':
